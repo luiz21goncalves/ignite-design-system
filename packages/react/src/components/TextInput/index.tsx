65c56e222da5f47bc9baf52498279b6de31c099c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-10-08 20:51:25 -0300

    feat(react): add text input component

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from 'react'
+import * as S from './styles'
+
+export type TextInputProps = ComponentProps<typeof S['Input']> & {
+  prefix?: string
+}
+
+export function TextInput(props: TextInputProps) {
+  const { prefix, ...attrs } = props
+
+  const hasPrefix = Boolean(prefix)
+
+  return (
+    <S.TextContainer>
+      {hasPrefix && <S.Prefix>{prefix}</S.Prefix>}
+      <S.Input {...attrs} />
+    </S.TextContainer>
+  )
+}
