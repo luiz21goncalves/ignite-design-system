c8465057233f1ebc37b2a11295913762530cedfe
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-10-08 11:11:06 -0300

    feat: add button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,34 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.gray100 }}>Hello World</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  color: '$white',
+  fontWeight: '$bold',
+
+  '&:hover': {
+    backgroundColor: '$ignite500',
+  },
+
+  variants: {
+    size: {
+      small: {
+        padding: '$2 $4',
+        fontSize: '$sm',
+      },
+      big: {
+        padding: '$3 $6',
+        fontSize: '$md',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
