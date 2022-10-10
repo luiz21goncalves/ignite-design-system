08343cf490ab9067cd2ca8049c2e2b25db4c66d0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-10-10 18:14:16 -0300

    feat(react): add checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
+
+export const Selected: StoryObj<CheckboxProps> = {
+  args: {
+    defaultChecked: true,
+  },
+}
---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { Check } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import * as S from './styles'
+
+export type CheckboxProps = ComponentProps<typeof S['CheckboxContainer']>
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <S.CheckboxContainer {...props}>
+      <S.CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </S.CheckboxIndicator>
+    </S.CheckboxContainer>
+  )
+}
